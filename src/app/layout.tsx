56b1ee1bd760a98377ca354e0e1838628054109d
jeetnik <saijeet01@gmail.com>
2025-02-21 20:08:04 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,33 +2,39 @@ import type { Metadata } from "next";
 import RootLayout from "./root-layout";
 
 export const metadata: Metadata = {
-  title: "Solspher | Solana Token Launchpad",
+  title: "Solspher | Solana Token Launchpad for Seamless Token Creation",
   description:
-    "Solspher is a powerful Solana Token Launchpad that enables seamless token creation, fundraising, and management on the Solana blockchain.",
+    "Launch Solana-based tokens effortlessly with Solspher. Secure, fast, and user-friendly platform for token creation, fundraising, and management.",
   openGraph: {
-    title: "Solspher | Solana Token Launchpad",
+    title: "Solspher | Solana Token Launchpad for Seamless Token Creation",
     description:
-      "Create and launch your Solana-based token effortlessly with Solspher. Secure, fast, and user-friendly.",
-    url: "https://yourwebsite.com",
+      "Solspher makes launching Solana-based tokens easy and secure. Start your crypto journey today!",
+    url: "https://solsphere-rosy.vercel.app/",
     images: [
       {
-        url: "https://yourwebsite.com/solspher-preview.jpg",
+        url: "https://solsphere-rosy.vercel.app/solspher-preview.jpg",
         width: 1200,
         height: 630,
+        alt: "Solspher - The Best Solana Token Launchpad",
       },
     ],
+    siteName: "Solspher",
+    type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Solspher | Solana Token Launchpad",
+    title: "Solspher | Solana Token Launchpad for Seamless Token Creation",
     description:
       "Solspher makes launching Solana-based tokens easy and secure. Start your crypto journey today!",
-    images: ["https://yourwebsite.com/solspher-preview.jpg"],
-  },other: {
+    images: ["https://solsphere-rosy.vercel.app/solspher-preview.jpg"],
+  },
+  alternates: {
+    canonical: "https://solsphere-rosy.vercel.app/",
+  },
+  other: {
     "google-site-verification": "M_46EfodvjRbZ_6fI9JmFbDgrVzKB20nO14B7b2D00o",
   },
 };
-
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <RootLayout>{children}</RootLayout>;
